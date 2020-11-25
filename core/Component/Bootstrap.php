fe5bef7afbbaaa25525a99c0ce1109bbc7556062
Bagus A Priambada <iam.adty@gmail.com>
2020-11-25 13:40:16 +0700

    bootstrap with decorator

--- core/Component/Bootstrap.php
+++ core/Component/Bootstrap.php
@@ -1,24 +1,26 @@
 <?php namespace PWC\Component;
 
-use PWC\AssetsManager\Config as AssetConfig;
 use PWC\BuilderTrait;
 use PWC\Component;
 use PWC\Component\Html\Meta;
 use PWC\Component\Html\Script;
 use PWC\Component\Html\Style;
+use PWC\Component\Html\Title;
+use PWC\Config\Asset;
 
 class Bootstrap extends Component
 {
     protected $_ID = 'pwc-bootstrap';
-    protected $title = 'Bootstrap';
-    protected $html = [];
-    protected $head = [];
-    protected $body = [];
-    protected $jquery = [];
+
+    protected ?JQuery $jquery;
 
     public function render(): string
     {
-        return (string) JQuery::build(
+        return (string) ($this->jquery ?? JQuery::build(
+            Title::build(
+                Text::build('Bootstrap')
+            )->withDecorators($this->_decorators)->asDecorator()->replace(),
+
             Meta::register([
                 'charset' => 'utf-8',
             ]),
@@ -30,15 +32,12 @@ class Bootstrap extends Component
                 'name' => 'viewport',
                 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no'
             ]),
-            Style::register(AssetConfig::get('dir') . 'php-web-component/bootstrap/css/bootstrap.min.css'),
+            Style::register(Asset::get('dir') . 'php-web-component/bootstrap/css/bootstrap.min.css'),
             parent::render(),
-            Script::register(AssetConfig::get('dir') . 'php-web-component/bootstrap/js/bootstrap.min.js'),
-        )->config([
-            'title' => $this->title,
-            'html' => $this->html,
-            'head' => $this->head,
-            'body' => $this->body,
-        ])->decorate($this->jquery);
+            Script::register(Asset::get('dir') . 'php-web-component/bootstrap/js/bootstrap.min.js'),
+
+            ...$this->_decorators->get(),
+        ))->withDecorators($this->_decorators);
     }
 
     use BuilderTrait;
